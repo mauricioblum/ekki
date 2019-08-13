ae15a5febadb30418645256074b7bea5c55a2e19
Mauricio Blum <mauriciocblum@gmail.com>
2019-08-13 17:04:35 -0300

    Added api and cors domains

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:3333',
+  baseURL: window.location.host.includes('ekki')
+    ? 'https://ekki-backend.herokuapp.com'
+    : 'http://127.0.0.1:3333',
 });
 
 export default api;
